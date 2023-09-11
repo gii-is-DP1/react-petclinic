54cdb93a73c73a4f731896de40b69b21e29e0130
José Antonio Parejo Maestre <japarejo@gmail.com>
2023-09-11 17:04:56 +0200

    updated securty configuration to the latest functional dialect in Spring Security 6

--- src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
@@ -1,5 +1,12 @@
 package org.springframework.samples.petclinic.configuration;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,23 +17,15 @@ import org.springframework.samples.petclinic.configuration.jwt.AuthEntryPointJwt
 import org.springframework.samples.petclinic.configuration.jwt.AuthTokenFilter;
 import org.springframework.samples.petclinic.configuration.services.UserDetailsServiceImpl;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 @Configuration
 @EnableWebSecurity
@@ -59,35 +58,34 @@ public class SecurityConfiguration {
 	@Bean
 	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeHttpRequests(authorizeRequests ->	authorizeRequests
-				.requestMatchers("/resources/**", "/webjars/**", "/h2-console/**", "/static/**", "/swagger-resources/**").permitAll()
-				.requestMatchers( "/api/v1/clinics","/", "/oups","/api/v1/auth/**","/v3/api-docs/**","/swagger-ui.html","/swagger-ui/**").permitAll()												
-				.requestMatchers("/api/v1/plan").hasAuthority("OWNER")
-				.requestMatchers("/api/v1/users/**").hasAuthority(ADMIN)
-				.requestMatchers("/api/v1/clinicOwners/all").hasAuthority(ADMIN)
-				.requestMatchers("/api/v1/clinicOwners/**").hasAnyAuthority(ADMIN, CLINIC_OWNER)
-				.requestMatchers(HttpMethod.DELETE, "/api/v1/consultations/**").hasAuthority(ADMIN)
-				.requestMatchers("/api/v1/owners/*/pets/**").authenticated()				
-				.requestMatchers("/api/v1/owners/**").hasAuthority(ADMIN)
-				.requestMatchers("/api/v1/visits/**").authenticated()
-				.requestMatchers(HttpMethod.GET, "/api/v1/pets/stats").hasAuthority(ADMIN)
-				.requestMatchers("/api/v1/pets").authenticated()
-				.requestMatchers("/api/v1/pets/**").authenticated()
-				.requestMatchers("/api/v1/clinics/**").hasAnyAuthority(CLINIC_OWNER, ADMIN)
-				.requestMatchers(HttpMethod.GET, "/api/v1/vets/stats").hasAuthority(ADMIN)
-				.requestMatchers(HttpMethod.GET, "/api/v1/vets/**").authenticated()
-				.requestMatchers("/api/v1/vets/**").hasAnyAuthority(ADMIN, "VET", CLINIC_OWNER) 
-				.anyRequest().authenticated());
 		http
-			.cors()
-		.and()
-			.csrf().disable()		
-			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
-		.and()
-			.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.headers().frameOptions().sameOrigin();
-		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+			.cors(withDefaults())		
+			.csrf(AbstractHttpConfigurer::disable)		
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))			
+			.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.sameOrigin()))
+			.exceptionHandling((exepciontHandling) -> exepciontHandling.authenticationEntryPoint(unauthorizedHandler))			
+			
+			.authorizeHttpRequests(authorizeRequests ->	authorizeRequests
+			.requestMatchers("/resources/**", "/webjars/**", "/h2-console/**", "/static/**", "/swagger-resources/**").permitAll()
+			.requestMatchers( "/api/v1/clinics","/", "/oups","/api/v1/auth/**","/v3/api-docs/**","/swagger-ui.html","/swagger-ui/**").permitAll()												
+			.requestMatchers("/api/v1/plan").hasAuthority("OWNER")
+			.requestMatchers("/api/v1/users/**").hasAuthority(ADMIN)
+			.requestMatchers("/api/v1/clinicOwners/all").hasAuthority(ADMIN)
+			.requestMatchers("/api/v1/clinicOwners/**").hasAnyAuthority(ADMIN, CLINIC_OWNER)
+			.requestMatchers(HttpMethod.DELETE, "/api/v1/consultations/**").hasAuthority(ADMIN)
+			.requestMatchers("/api/v1/owners/*/pets/**").authenticated()				
+			.requestMatchers("/api/v1/owners/**").hasAuthority(ADMIN)
+			.requestMatchers("/api/v1/visits/**").authenticated()
+			.requestMatchers(HttpMethod.GET, "/api/v1/pets/stats").hasAuthority(ADMIN)
+			.requestMatchers("/api/v1/pets").authenticated()
+			.requestMatchers("/api/v1/pets/**").authenticated()
+			.requestMatchers("/api/v1/clinics/**").hasAnyAuthority(CLINIC_OWNER, ADMIN)
+			.requestMatchers(HttpMethod.GET, "/api/v1/vets/stats").hasAuthority(ADMIN)
+			.requestMatchers(HttpMethod.GET, "/api/v1/vets/**").authenticated()
+			.requestMatchers("/api/v1/vets/**").hasAnyAuthority(ADMIN, "VET", CLINIC_OWNER) 
+			.anyRequest().authenticated())					
+			
+			.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 		// http.addFilterAfter(new SpaWebFilter(),
 		// UsernamePasswordAuthenticationFilter.class);
 		return http.build();
