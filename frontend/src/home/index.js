728d2e227156686f0bb856fecdb94457b68ff935
us-invest-personal <us.invest.personal.2020@gmail.com>
2023-09-17 17:40:39 +0200

    Fixes #5

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../App.css';
 import '../static/css/home/home.css';
+import logo from '../static/images/logonuevo.png'
 
 export default function Home(){
     return(
@@ -8,6 +9,7 @@ export default function Home(){
             <div className="hero-div">
                 <h1>Petclinic</h1>
                 <h3>---</h3>
+                <img src={logo}/> 
                 <h3>Find the best vet for your pet</h3>
             </div>
         </div>
