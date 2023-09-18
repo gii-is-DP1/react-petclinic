1cb64754c74fca3016d1ebfb9e80ccc963fcfb93
José Antonio Parejo Maestre <japarejo@gmail.com>
2023-09-18 09:51:20 +0200

    Removed spuerous dependency

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import '../static/css/home/home.css';
+import '../static/css/home/home.css'; 
 
 export default function Home(){
     return(
@@ -8,7 +8,7 @@ export default function Home(){
             <div className="hero-div">
                 <h1>Petclinic</h1>
                 <h3>---</h3>
-                <h3>Find the best vet for your pet</h3>
+                <h3>Find the best vet for your pet</h3>                
             </div>
         </div>
     );
