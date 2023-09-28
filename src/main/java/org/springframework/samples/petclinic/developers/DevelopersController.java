c25fff968837af840045992b613201729a73be5b
us-invest-personal <us.invest.personal.2020@gmail.com>
2023-09-29 01:35:39 +0200

    Configuración Ejercicio II - 1.3
    DevelopersController

--- src/main/java/org/springframework/samples/petclinic/developers/DevelopersController.java
+++ src/main/java/org/springframework/samples/petclinic/developers/DevelopersController.java
@@ -9,12 +9,16 @@ import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.samples.petclinic.model.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("/api/v1/developers")
 public class DevelopersController {
     List<Developer> developers;
 
-
+    @GetMapping
     public List<Developer> getDevelopers(){
         if(developers==null)
             loadDevelopers();
