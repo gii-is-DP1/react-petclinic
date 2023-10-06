f9abf6e20cbce81ee09a1931eb33525260d84fb4
Jose Antonio Parejo Maestre <japarejo@gmail.com>
2023-10-06 17:44:40 +0200

    Update useFetchState.js
    Now the only required parameters are initial and url.

--- frontend/src/util/useFetchState.js
+++ frontend/src/util/useFetchState.js
@@ -6,17 +6,20 @@ export default function useFetchState(initial, url, jwt, setMessage, setVisible,
         if (url) {
             if (!id || id !== "new") {
                 let ignore = false;
-                fetch(url, {
+                fetch(url, jwt?{
                     headers: {
                         "Authorization": `Bearer ${jwt}`,
                     },
-                })
+                }:{})
                     .then(response => response.json())
                     .then(json => {
                         if (!ignore) {
                             if (json.message) {
-                                setMessage(json.message);
-                                setVisible(true);
+                                if(setMessage!==null){
+                                    setMessage(json.message);
+                                    setVisible(true);
+                                }else
+                                    window.alert(json.message);
                             }
                             else {
                                 setData(json);
