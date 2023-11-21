b4026e98c21550f30cb8ed918311a76bb34d9c1f
José Antonio Parejo Maestre <japarejo@gmail.com>
2023-11-21 12:02:28 +0100

    Controller Test

---
+++ src/test/java/org/springframework/samples/petclinic/movies/CustomerControllerTest.java
@@ -0,0 +1,48 @@
+package org.springframework.samples.petclinic.movies;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class CustomerControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
+
+    @Test
+    @Transactional
+    public void testShowStatement() throws Exception {
+
+        mockMvc.perform(get("/api/v1/customers/1/statement"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.customer.name").value("Juan"))
+                .andExpect(jsonPath("$.totalAmount").value("18.5"))
+			    .andExpect(jsonPath("$.frequentRenterPoints").value("4"));
+    }    
+
+}
